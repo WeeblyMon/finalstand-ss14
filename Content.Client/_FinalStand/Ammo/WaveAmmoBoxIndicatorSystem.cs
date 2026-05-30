908e5e8f168d38ae586fbdd61937370012974e3a
Gary Chen <gurugaryc@gmail.com>
2026-05-30 22:13:59 +0100

    Ammo box: wave indicator overlay and ammo box entity

---
+++ Content.Client/_FinalStand/Ammo/WaveAmmoBoxIndicatorSystem.cs
@@ -0,0 +1,27 @@
+using Robust.Client.Graphics;
+
+namespace Content.Client._FinalStand.Ammo;
+
+public sealed class WaveAmmoBoxIndicatorSystem : EntitySystem
+{
+    [Dependency] private readonly IOverlayManager _overlayManager = default!;
+
+    private WaveAmmoBoxIndicatorOverlay? _overlay;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _overlay = new WaveAmmoBoxIndicatorOverlay();
+        _overlayManager.AddOverlay(_overlay);
+    }
+
+    public override void Shutdown()
+    {
+        base.Shutdown();
+        if (_overlay != null)
+        {
+            _overlayManager.RemoveOverlay(_overlay);
+            _overlay = null;
+        }
+    }
+}
