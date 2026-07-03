221f5ada5cea471957dd4e828e540ecce6336473
Gary Chen <gurugaryc@gmail.com>
2026-07-03 19:27:48 +0100

    Money Donation System

---
+++ Content.Client/_FinalStand/CashTransfer/FSCashTransferBoundUserInterface.cs
@@ -0,0 +1,40 @@
+using Content.Client._FinalStand.CashTransfer.UI;
+using Content.Shared._FinalStand.CashTransfer;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._FinalStand.CashTransfer;
+
+public sealed class FSCashTransferBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private CashTransferWindow? _window;
+
+    public FSCashTransferBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey) { }
+
+    protected override void Open()
+    {
+        base.Open();
+        _window = this.CreateWindow<CashTransferWindow>();
+        _window.OnTransferConfirmed += OnTransferConfirmed;
+        _window.OnClose += Close;
+    }
+
+    private void OnTransferConfirmed(int amount)
+    {
+        SendMessage(new FSCashTransferRequestMessage(amount));
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        if (state is FSCashTransferBuiState s)
+            _window?.UpdateState(s);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (!disposing)
+            return;
+        _window?.Dispose();
+    }
+}
