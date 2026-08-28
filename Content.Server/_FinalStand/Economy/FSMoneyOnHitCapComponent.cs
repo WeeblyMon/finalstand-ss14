e978f2a03d9f893ec2e5a5e782d5b5545b7a0875
Gary Chen <gurugaryc@gmail.com>
2026-08-28 01:32:51 +0100

    fix corpse money farming

--- Content.Server/_FinalStand/Economy/FSMoneyOnHitCapComponent.cs
+++ Content.Server/_FinalStand/Economy/FSMoneyOnHitCapComponent.cs
@@ -3,6 +3,6 @@ namespace Content.Server._FinalStand.Economy;
 [RegisterComponent]
 public sealed partial class FSMoneyOnHitCapComponent : Component
 {
-    [DataField] public int MaxMoneyPerPlayer = 450;
+    [DataField] public int MaxMoneyPerPlayer = 200;
     public Dictionary<EntityUid, int> MoneyGivenPerPlayer = new();
 }
