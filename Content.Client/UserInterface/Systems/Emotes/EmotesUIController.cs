c3100770ab4baa18b9c87f391d02c593f9ba12bc
Gary Chen <gurugaryc@gmail.com>
2026-08-31 10:14:03 +0100

    give the goob fart emotes a menu category instead of crashing the wheel

--- Content.Client/UserInterface/Systems/Emotes/EmotesUIController.cs
+++ Content.Client/UserInterface/Systems/Emotes/EmotesUIController.cs
@@ -33,6 +33,8 @@ public sealed partial class EmotesUIController : UIController, IOnStateChanged<G
                 new SpriteSpecifier.Rsi(new ResPath("/Textures/Clothing/Hands/Gloves/latex.rsi"), "icon")),
             [EmoteCategory.Vocal] = ("emote-menu-category-vocal",
                 new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Emotes/vocal.png"))),
+            [EmoteCategory.Farts] = ("emote-menu-category-farts", // Goobstation - Fart Emotes
+                new SpriteSpecifier.Texture(new ResPath("/Textures/_Goobstation/Interface/Emotes/fart.png"))),
         };
 
     public void OnStateEntered(GameplayState state)
@@ -169,21 +171,24 @@ public sealed partial class EmotesUIController : UIController, IOnStateChanged<G
             list.Add(actionOption);
         }
 
-        var models = new RadialMenuOptionBase[emotesByCategory.Count];
-        var i = 0;
+        var models = new List<RadialMenuOptionBase>(emotesByCategory.Count);
         foreach (var (key, list) in emotesByCategory)
         {
-            var tuple = EmoteGroupingInfo[key];
+            // A category with no grouping entry would otherwise take the whole emote menu down.
+            if (!EmoteGroupingInfo.TryGetValue(key, out var tuple))
+            {
+                Log.Warning($"No emote grouping info for category '{key}'; its emotes are hidden from the menu.");
+                continue;
+            }
 
-            models[i] = new RadialMenuNestedLayerOption(list)
+            models.Add(new RadialMenuNestedLayerOption(list)
             {
                 IconSpecifier = RadialMenuIconSpecifier.With(tuple.Sprite),
                 ToolTip = Loc.GetString(tuple.Tooltip)
-            };
-            i++;
+            });
         }
 
-        return models;
+        return models.ToArray();
     }
 
     private void HandleRadialButtonClick(EmotePrototype prototype)
